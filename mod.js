25497a4e2f04e9d0be262b3b1d844c404a19b8bb
Sonoko Mizuki <sonoko@mizuki.io>
2021-03-30 18:21:35 +0900

    Update

--- mod.js
+++ mod.js
@@ -24,6 +24,8 @@ function handleRequest(request) {
     // Use stringify function to convert javascript object to JSON string.
     const json = JSON.stringify({
       message: "Hello from Deno Deploy",
+      name: "MizukiSonoko",
+      github: "https://github.com/MizukiSonoko",
     });
 
     return new Response(json, {
@@ -38,12 +40,12 @@ function handleRequest(request) {
       align="center"
       style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
     >
-      <h1>Return JSON and/or HTML Example</h1>
+      <h1>Hello! I'm Sonoko Mizuki.</h1>
       <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
+        <a href="/html">/html</a>
       </p>
       <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
+        <a href="/json">/json</a>
       </p>
     </body>`,
     {
